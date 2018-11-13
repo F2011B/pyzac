139dd72606a7c2051c881a8976113c092f5cb87e
Dark Light <darkshadow201710[at]gmx.de>
2018-11-13 19:47:45 +0100

    Started to implement state handling

--- src/pyzac/pyzac_base.py
+++ src/pyzac/pyzac_base.py
@@ -1,23 +1,57 @@
 import zmq
 from multiprocessing import Process
 import functools
+import inspect
 
 started_processes = list()
 
 
-def _wrap_pyzmq(func, pub_addr, sub_addr):
+def get_pyzac_state(func):
+    signature = inspect.signature(func)
+    for k, v in signature.parameters.items():
+        if k == "pyzac_state":
+            return v.default
+
+
+def try_get_default_state(func):
+    funcargs = inspect.getargspec(func).args
+    usestate = "pyzac_state" in funcargs
+    if usestate:
+        return True, get_pyzac_state(func)
+    else:
+        return False, ""
+
+
+def _wrap_pyzmq(func, pub_addr="", sub_addr=""):
+    nopub = pub_addr == ""
+    nosub = sub_addr == ""
+    if nopub and nosub:
+        return
+
     context = zmq.Context()
-    sock_sub = context.socket(zmq.SUB)
-    sock_sub.connect(sub_addr)
-    sock_pub = context.socket(zmq.PUB)
-    sock_pub.connect(pub_addr)
+
+    if not nosub:
+        sock_sub = context.socket(zmq.SUB)
+        sock_sub.connect(sub_addr)
+    if not nopub:
+        sock_pub = context.socket(zmq.PUB)
+        sock_pub.connect(pub_addr)
+
+    hasstate, state = try_get_default_state(func)
     while True:
-        func_pars = sock_sub.recv_pyobj()
-        func_res = func(**func_pars)
+        if not nosub:
+            func_pars = sock_sub.recv_pyobj()
+
+        if hasstate:
+            func_res = func(pyzac_state=state, **func_pars)
+            state = func_res
+        else:
+            func_res = func(**func_pars)
+
         sock_pub.send_pyobj(func_res)
 
 
-def pyzac_decorator(pub_addr, sub_addr):
+def pyzac_decorator(pub_addr="", sub_addr=""):
     def decorator_pyzeromq(func):
         @functools.wraps(func)
         def wrapper_process(*args, **kwargs):
