78ac861d3104a0df68b6e9fb40248da6da8b0261
Dark Light <darkshadow201710[at]gmx.de>
2018-11-17 16:47:28 +0100

    state handling is now implemented and gets tested.

--- test/test_pubsub.py
+++ test/test_pubsub.py
@@ -20,7 +20,33 @@ def test_decorators():
         p.join()
 
 
-if __name__ == "__main__":
+def test_state():
+    count = 0
+    std_pub_val = 20
 
-    test_decorators()
-    print("Processes stoped")
+    @pyzac_decorator(pub_addr="tcp://127.0.0.1:2000")
+    def publisher():
+        return std_pub_val
+
+    @pyzac_decorator(sub_addr="tcp://localhost:2000")
+    def subscriber(result, pyzac_state=0):
+        nonlocal count
+        if count == 1:
+            assert pyzac_state > 0
+            assert pyzac_state == std_pub_val
+        if count == 2:
+            assert pyzac_state == 2 * std_pub_val
+        count = count + 1
+        return result + pyzac_state
+
+    publisher()
+    subscriber()
+    sleep(1)
+    for p in started_processes:
+        p.terminate()
+        p.join()
+        print("Processes stoped")
+
+
+if __name__ == "__main__":
+    test_state()
