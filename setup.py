ddc1ad240252e115f96e69cf61871e22c725571e
Dark Light <darkshadow201710[at]gmx.de>
2018-11-26 20:41:21 +0100

    Made setup.py deployable over travis

--- setup.py
+++ setup.py
@@ -18,19 +18,28 @@ if sys.version_info < (3, 3):
     sys.exit(1)
 
 
+def long_description():
+    with open(os.path.join(BASE_PATH, "README.rst")) as readme:
+        result = readme.read()
+    result += "\n\n"
+    with open(os.path.join(BASE_PATH, "CHANGES.rst")) as changes:
+        result += changes.read()
+    return result
+
+
 setup(
     name="pyzac",
-    version="0.0.6",
+    version="0.0.7",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
     python_requires=">= 3.3",
-    license = "Apache 2.0",
+    license="Apache 2.0",
     # install_requires=["setuptools", "pip", "docutils", "purepng>=0.1.1"],
     # extras_require={"bitmap": ["Pillow"]},
     # setup_requires=["pytest-runner"],
     # tests_require=["pytest>=2.0.0", "pytest-assume", "requests", "PyPDF2"],
-    author="Dark Ligt aias FB2011Bc",
+    author="Dark Ligt alias FB2011B",
     description="The Python Actor Model using zeroMQ",
     long_description=open("README.rst").read(),
     url="https://github.com/F2011B/pyzac",
@@ -41,13 +50,11 @@ setup(
         "Environment :: Web Environment",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache 2.0",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Topic :: Functional Progamming",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
