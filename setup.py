5752ca6c90c5f3403b314ca5716b2fa9b6f7d48d
Dark Light <F2011B@users.noreply.github.com>
2019-03-11 20:23:21 +0100

    Updated patch version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def long_description():
 
 setup(
     name="pyzac",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
